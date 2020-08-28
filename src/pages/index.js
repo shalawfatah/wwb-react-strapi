0955e190337e87f23d9cbe244d49c06db2c4d4e5
Shalaw Fatah <shalaw.fatah@gmail.com>
2020-08-28 00:36:16 -0700

    netlify thing

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Shalaw</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>I will build this as my first project.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
