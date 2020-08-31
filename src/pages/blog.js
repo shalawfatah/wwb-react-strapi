3265495b88e3f7267c96053388506af68e01823f
Shalaw Fatah <shalaw.fatah@gmail.com>
2020-08-31 03:29:43 -0700

    front is ready

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,7 +5,7 @@ import Category from '../components/utilities/Category'
 import Sideline from '../components/utilities/Sideline'
 import SideWithoutSearch from '../components/utilities/SideWithoutSearch'
 import EducationCard from '../components/EducationCard'
-
+import Button from '../components/Button'
 
 
 const Blog = () => {
@@ -16,11 +16,14 @@ const Blog = () => {
             <Sideline  headline="Categories" subheadOne="Bee Programs" subheadTwo="Another title" subheadThree="Another Program" subheadFour="Final Title" subheadFive="Another Program" />
             <SideWithoutSearch headline="Top Posts" subheadOne="Blog Title 1" subheadTwo="Blog title" subheadThree="Another Program" subheadFour="Final Title" subheadFive="Another Program" />
             </div>
-            <EducationCard headline="Bee title" content="lorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsdlorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsd"></EducationCard>
-            <EducationCard headline="Bee title" content="lorem ipusm someone hi badsdsd"></EducationCard>
-            <EducationCard headline="Bee title" content="lorem ipusm someone hi badsdsd"></EducationCard>
-            <EducationCard headline="Bee title" content="lorem ipusm someone hi badsdsd"></EducationCard>
-            <EducationCard headline="Bee title" content="lorem ipusm someone hi badsdsd"></EducationCard>
+            <EducationCard headline="Bee title" content="lorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsdlorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsd" button={<Button text="Read More" />}></EducationCard>
+            <EducationCard headline="Bee title" content="lorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsdlorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsd" button={<Button text="Read More" />}></EducationCard>
+            <EducationCard headline="Bee title" content="lorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsdlorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsd" button={<Button text="Read More" />}></EducationCard>
+            <EducationCard headline="Bee title" content="lorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsdlorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsd" button={<Button text="Read More" />}></EducationCard>
+            <EducationCard headline="Bee title" content="lorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsdlorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsd" button={<Button text="Read More" />}></EducationCard>
+            <EducationCard headline="Bee title" content="lorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsdlorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsd" button={<Button text="Read More" />}></EducationCard>
+            <EducationCard headline="Bee title" content="lorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsdlorem ipusm someone hi badsdsd lorem ipusm someone hi badsdsd" button={<Button text="Read More" />}></EducationCard>
+
 
             </div>
         </Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
+
 const IndexPage = () => (
   <Layout>
     <div className="home-content mx-auto">
