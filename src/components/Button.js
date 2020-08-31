6213024393a3cad3cd135014340a90f2e44db8a4
Shalaw Fatah <shalaw.fatah@gmail.com>
2020-08-31 05:08:36 -0700

    added search button

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import {Link} from 'gatsby'
 
 const Button = ({text}) => {
     return (
-        <li className="list-none font-semibold float-left m-1 hero">
+        <li className="list-none font-semibold float-left m-4 hero">
                         <Link fade to='/' className="flex text-grey-700 bg-white py-2 px-4 focus:outline-none text-sm cursor-pointer button">{text}</Link>
         </li>
     )
