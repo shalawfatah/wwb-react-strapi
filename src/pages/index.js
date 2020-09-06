26b77d16533a3b020115b5f3ff6dcf2e8fca7a8d
Shalaw Fatah <shalaw.fatah@gmail.com>
2020-09-06 06:29:14 -0700

    added carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
 import BeeCard from '../components/utilities/BeeCard'
+import Slider from '../components/carousel/Slider'
 
 const IndexPage = () => (
   <Layout>
     <div className="home-content mx-auto lg:h-60">
       <BeeCard />
     </div>
+    <Slider />
   </Layout>
 )
 
