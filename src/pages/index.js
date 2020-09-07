f1d24c60761e63e364c40942cd6b7283b22e3ef6
Shalaw Fatah <shalaw.fatah@gmail.com>
2020-09-06 22:31:28 -0700

    added bee icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
-import React from "react"
+import React, {useState} from "react"
 import Layout from "../components/layout"
 import BeeCard from '../components/utilities/BeeCard'
 import Slider from '../components/carousel/Slider'
+import {GiBee} from 'react-icons/gi'
 
-const IndexPage = () => (
+const IndexPage = () => {
+  const [hideBee, setHideBee] = useState(false)
+  return (
   <Layout>
     <div className="home-content mx-auto lg:h-60">
-      <BeeCard />
+    {hideBee && <BeeCard />}
+      <GiBee className="text-yellow-500 text-5xl bee-icon hover:text-yellow-300 hover:text-6xl" onClick={()=> setHideBee(hideBee => !hideBee)} />
     </div>
     <Slider />
   </Layout>
-)
-
+  )
+}
 export default IndexPage
