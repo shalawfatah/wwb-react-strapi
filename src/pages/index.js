336824ef8e1fc10f89802cfa36479bfa4fb06abc
Shalaw Fatah <shalaw.fatah@gmail.com>
2020-09-07 08:23:14 -0700

    fixed map slider issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,18 @@ import Layout from "../components/layout"
 import BeeCard from '../components/utilities/BeeCard'
 import Slider from '../components/carousel/Slider'
 import {GiBee} from 'react-icons/gi'
+import MapImage from '../images/map-full-gray.png'
 
 const IndexPage = () => {
   const [hideBee, setHideBee] = useState(false)
   return (
   <Layout>
-    <div className="home-content mx-auto lg:h-60">
+    <div className="home-content mx-auto">
+    <img src={MapImage} alt="map iamge" />
+    <GiBee className="text-yellow-500 text-5xl bee-icon hover:text-yellow-300 hover:text-6xl" onClick={()=> setHideBee(hideBee => !hideBee)} />
+    <div className="bee-card-place">
     {hideBee && <BeeCard />}
-      <GiBee className="text-yellow-500 text-5xl bee-icon hover:text-yellow-300 hover:text-6xl" onClick={()=> setHideBee(hideBee => !hideBee)} />
+    </div>
     </div>
     <Slider />
   </Layout>
