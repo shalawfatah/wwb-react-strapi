299ca95b1001407bcdc375daa2600bc9423c1245
Shalaw Fatah <shalaw.fatah@gmail.com>
2020-11-11 20:28:32 -0800

    fully fixed subscribe button

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -23,7 +23,7 @@ const NavHeader = () => {
             })}
                 <AniLink fade to='/subscribe/' activeStyle={{backgroundColor: '#F08C00', color: '#FFF'}} className="flex text-grey-700 bg-white py-2 px-4 focus:outline-none text-sm cursor-pointer main-menu special-menu">Subscribe</AniLink>
             </div>
-            <FaIcons.FaBars className="text-3xl text-orange-500 cursor-pointer hamburger transition-all duration-500 ease-in-out" onClick={showSidebar} />
+            <FaIcons.FaBars className="text-5xl ml-2 text-orange-500 cursor-pointer hamburger transition-all duration-500 ease-in-out" onClick={showSidebar} />
             <div className={sidebar ? 'slider-menu transition-all duration-500 ease-in-out' : 'slider transition-all duration-500 ease-in-out'}>
             {links.map((link)=> {
                 return (
