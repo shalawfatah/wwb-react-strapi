e5080ab705f9d8eba0a582b1a2762cd5dda95177
shalawfatah <shalaw.fatah@gmail.com>
2020-12-03 18:04:16 -0800

    event fix

--- src/templates/event-template.js
+++ src/templates/event-template.js
@@ -12,7 +12,6 @@ import { format } from 'date-fns'
 
 const eventsTemplate = ({data}) => {
     const {summary, title, date, duration, location, photo, website, content, price, slug, available_quantity} = data.strapiEvents
-    const currently = `https://worldwidebee.org/events/${slug}`
 
     return (
         <Layout>
@@ -34,7 +33,7 @@ const eventsTemplate = ({data}) => {
                             <button className={`snipcart-add-item flex text-grey-700 bg-white py-2 px-4 focus:outline-none text-lg cursor-pointer button ${new Date(date) < new Date() ? 'disable-btn' : ''}`}
                             data-item-id={title}
                             data-item-price={price}
-                            data-item-url={currently}
+                            data-item-url={`/events/${slug}`}
                             data-item-description={summary}
                             data-item-name={title}
                             data-item-max-quantity={available_quantity}
