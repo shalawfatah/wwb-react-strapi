e1c497f05d6d8d0dc07c38682fea52c6d597af17
shalawfatah <shalaw.fatah@gmail.com>
2020-12-21 22:30:12 -0800

    event url problem

--- src/templates/event-template.js
+++ src/templates/event-template.js
@@ -12,7 +12,7 @@ import { format } from 'date-fns'
 
 const eventsTemplate = ({data}) => {
     const {summary, title, date, duration, location, photo, website, content, price, slug, available_quantity} = data.strapiEvents
-
+    const myUrl = location.pathname
     return (
         <Layout>
             <SEO title={title} description={summary} />
@@ -33,7 +33,7 @@ const eventsTemplate = ({data}) => {
                             <button className={`snipcart-add-item flex text-grey-700 bg-white py-2 px-4 focus:outline-none text-lg cursor-pointer button ${new Date(date) < new Date() ? 'disable-btn' : ''}`}
                             data-item-id={title}
                             data-item-price={price}
-                            data-item-url={`https://www.worldwidebee.org/events/${slug}`}
+                            data-item-url={myUrl}
                             data-item-description={summary}
                             data-item-name={title}
                             data-item-max-quantity={available_quantity}
